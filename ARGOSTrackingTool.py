9d94c560114cb237a2451fcfed42d1949efdf811
ranik8 <rani.kumar@duke.edu>
2020-09-23 09:07:48 -0400

    Initial commit of ARGOS script

---
+++ ARGOSTrackingTool.py
@@ -0,0 +1,12 @@
+or: rk272
+"""
+#-------------------------------------------------------------
+# ARGOSTrackingTool.py
+#
+# Description: Reads in an ARGOS tracking data file and allows
+#   the user to view the location of the turtle for a specified
+#   date entered via user input.
+#
+# Author: Rani Kumar (rani.kumar@duke.edu)
+# Date:   Fall 2020
+#--------------------------------------------------------------
