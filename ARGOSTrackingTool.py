339c07c3fbf206452c07bae4dd66bebf781c6688
ranik8 <rani.kumar@duke.edu>
2020-09-30 07:27:25 -0400

    Parses a pasted data line into variables

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -1,5 +1,3 @@
-or: rk272
-"""
 #-------------------------------------------------------------
 # ARGOSTrackingTool.py
 #
@@ -10,3 +8,21 @@ or: rk272
 # Author: Rani Kumar (rani.kumar@duke.edu)
 # Date:   Fall 2020
 #--------------------------------------------------------------
+
+
+#Pretend we read one line of data from the file
+lineString= '20616	29051	7/3/2003 9:13	3	66	33.898	-77.958	27.369	-46.309	6	0	-126	529	3	401 651134.7	0'
+
+
+#Split the string in ot a list of data items
+lineData = lineString.split()
+
+#Extract items in the list into variables
+record_id = lineData[0]
+obs_date = lineData[2]
+obs_lc = lineData[4]
+obs_lat = lineData[6]
+obs_lon = lineData[7]
+
+#Print the location of sara
+print(f"Record {record_id} indicates Sara was seen at lat:{obs_lat}, lon:{obs_lon} on {obs_date}")
