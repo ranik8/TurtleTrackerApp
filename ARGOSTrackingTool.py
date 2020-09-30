121a4999ccb12e022fccf4d5015b89a93f1911c0
ranik8 <rani.kumar@duke.edu>
2020-09-30 18:23:47 -0400

    Displays location for user date

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -9,6 +9,9 @@
 # Date:   Fall 2020
 #--------------------------------------------------------------
 
+#Ask user for the search date
+user_date = input("Enter date to search for Sara:")
+
 #Create a variable pointing to a data file
 file_name= './data/raw/sara.txt'
 
@@ -45,4 +48,22 @@ for lineString in line_list:
     if obs_lc in ('1','2','3'): 
         date_dict[record_id]= obs_date
         coord_dict[record_id]= (obs_lat,obs_lon)
+
+#create empty list to hold matching keys
+matching_keys = []
     
+
+#loop through items in the date_dict,and collect keys for matching ones
+for date_item in date_dict.items():
+    #get the key and date of the dictionary item
+    the_key, the_date= date_item
+    #see if the date matches the user date
+    if the_date == user_date:
+        #if so, add the key to the list 
+        matching_keys.append(the_key)
+
+#reveal  locations for each key in matching keys
+for matching_key in matching_keys:
+        obs_lat, obs_lon = coord_dict[matching_key]
+        obs_date = date_dict[matching_key]
+        print(f"Record {matching_key} indicates Sara was seen at lat:{obs_lat}, lon:{obs_lon} on {obs_date}")
